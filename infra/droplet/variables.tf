2e7e8d1228b294f947e85f4e4017c915e7ca55f5
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-06 16:42:32 -0700

    Update variables.tf

--- infra/droplet/variables.tf
+++ infra/droplet/variables.tf
@@ -1,16 +1,64 @@
-variable "do_token"             { type = string; sensitive = true }
-variable "ssh_fingerprint"      { type = string }
-variable "droplet_name"         { type = string }
-variable "region"               { type = string; default = "nyc3" }
-variable "size"                 { type = string; default = "s-1vcpu-2gb" }
+################################################################################
+#  infra/droplet/variables.tf
+#  All variables used by the Axialy Admin droplet module
+################################################################################
 
-variable "repo_url"             { type = string }
+variable "do_token" {
+  type      = string
+  sensitive = true
+}
 
-variable "db_host"              { type = string }
-variable "db_port"              { type = string }
-variable "db_user"              { type = string }
-variable "db_pass"              { type = string; sensitive = true }
+variable "ssh_fingerprint" {
+  type = string
+}
 
-variable "admin_default_user"      { type = string; sensitive = true }
-variable "admin_default_email"     { type = string; sensitive = true }
-variable "admin_default_password"  { type = string; sensitive = true }
+variable "region" {
+  type    = string
+  default = "nyc3"
+}
+
+variable "size" {
+  type    = string
+  default = "s-1vcpu-2gb"
+}
+
+variable "droplet_name" {
+  type = string
+}
+
+variable "repo_url" {
+  type = string
+}
+
+# ---------- DB connection for Axialy_UI & Axialy_ADMIN ----------
+variable "db_host" {
+  type = string
+}
+
+variable "db_port" {
+  type    = string
+  default = "25060"            # DO managed-MySQL default
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_pass" {
+  type      = string
+  sensitive = true
+}
+
+# ---------- first-use admin credentials ----------
+variable "admin_default_user" {
+  type = string
+}
+
+variable "admin_default_email" {
+  type = string
+}
+
+variable "admin_default_password" {
+  type      = string
+  sensitive = true
+}
