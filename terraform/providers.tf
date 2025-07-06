c90603049526624553d452254bd07e06807621e8
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-06 12:58:22 -0700

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+provider "random" {}
