1365f3c82c544eb0e00b4ed77dd4ad5d6675fc2c
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-06 23:54:56 -0700

    Update main.tf

--- infra/droplet/main.tf
+++ infra/droplet/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -11,31 +12,36 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+# Data source to get the SSH key
 data "digitalocean_ssh_key" "this" {
   name = var.ssh_key_name
 }
 
+# The Axialy Admin droplet
 resource "digitalocean_droplet" "admin" {
   name   = var.droplet_name
   region = var.region
   size   = var.size
   image  = "ubuntu-24-04-x64"
-
+  
   ssh_keys   = [data.digitalocean_ssh_key.this.id]
   ipv6       = false
-  monitoring = false
+  monitoring = true
   backups    = false
-
+  
+  # Use the template file to generate user_data
   user_data = templatefile("${path.module}/user_data.tpl", {
     repo_url               = var.repo_url
-
     db_host                = var.db_host
     db_port                = var.db_port
     db_user                = var.db_user
     db_pass                = var.db_pass
-
     admin_default_user     = var.admin_default_user
     admin_default_email    = var.admin_default_email
     admin_default_password = var.admin_default_password
   })
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 }
