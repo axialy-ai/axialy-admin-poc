cfc3d86e18ef802f9ebbdf88c3f60819da79d695
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-06 23:02:26 -0700

    Update main.tf

--- infra/droplet/main.tf
+++ infra/droplet/main.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -13,38 +11,34 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# --------------------------------------------------------------------
-#  Re-use the key that’s already in the DigitalOcean account
-# --------------------------------------------------------------------
-data "digitalocean_ssh_key" "default" {
+data "digitalocean_ssh_key" "this" {
   name = var.ssh_key_name
 }
 
-# --------------------------------------------------------------------
-#  Admin Droplet
-# --------------------------------------------------------------------
 resource "digitalocean_droplet" "admin" {
-  name     = var.droplet_name
-  region   = var.region
-  size     = var.size
-  image    = "ubuntu-24-04-x64"
-  ssh_keys = [data.digitalocean_ssh_key.default.id]
+  name              = var.droplet_name
+  region            = var.region
+  size              = var.size
+  image             = "ubuntu-24-04-x64"
+  ssh_keys          = [data.digitalocean_ssh_key.this.id]
+  monitoring        = false
+  ipv6              = false
+  backups           = false
 
-  user_data = templatefile("${path.module}/cloud-init.yaml", {
-    repo_url               = var.repo_url
-    db_host                = var.db_host
-    db_port                = var.db_port
-    db_user                = var.db_user
-    db_pass                = var.db_pass
-    admin_default_user     = var.admin_default_user
-    admin_default_email    = var.admin_default_email
-    admin_default_password = var.admin_default_password
+  user_data = templatefile("${path.module}/user_data.tpl", {
+    repo_url                = var.repo_url
+
+    db_host                 = var.db_host
+    db_port                 = var.db_port
+    db_user                 = var.db_user
+    db_pass                 = var.db_pass
+
+    admin_default_user      = var.admin_default_user
+    admin_default_email     = var.admin_default_email
+    admin_default_password  = var.admin_default_password
   })
 }
 
-# --------------------------------------------------------------------
-#  Outputs
-# --------------------------------------------------------------------
 output "droplet_ip" {
   value = digitalocean_droplet.admin.ipv4_address
 }
