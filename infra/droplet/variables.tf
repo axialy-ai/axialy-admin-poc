5e377fc14118aaa236d0a1a5491f1300d848cba5
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-06 21:12:08 -0700

    Update variables.tf

--- infra/droplet/variables.tf
+++ infra/droplet/variables.tf
@@ -1,19 +1,19 @@
-variable "do_token"              { type = string }
+variable "do_token"               { type = string }
 
-# Fingerprint of the **existing** SSH key in DigitalOcean
-variable "ssh_fingerprint"       { type = string }
+# Name of the *existing* SSH key in DigitalOcean
+variable "ssh_key_name"           { type = string }
 
-variable "droplet_name"          { type = string }
-variable "region"                { type = string }
-variable "size"                  { type = string }
+variable "droplet_name"           { type = string }
+variable "region"                 { type = string }
+variable "size"                   { type = string }
 
-variable "repo_url"              { type = string }
+variable "repo_url"               { type = string }
 
-variable "db_host"               { type = string }
-variable "db_port"               { type = string }
-variable "db_user"               { type = string }
-variable "db_pass"               { type = string }
+variable "db_host"                { type = string }
+variable "db_port"                { type = string }
+variable "db_user"                { type = string }
+variable "db_pass"                { type = string }
 
-variable "admin_default_user"    { type = string }
-variable "admin_default_email"   { type = string }
-variable "admin_default_password"{ type = string }
+variable "admin_default_user"     { type = string }
+variable "admin_default_email"    { type = string }
+variable "admin_default_password" { type = string }
