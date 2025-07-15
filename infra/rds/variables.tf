3a1ef5a36be8561dd9efd0cf8aa5e47f08cecd5b
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-14 23:06:22 -0700

    Create variables.tf

---
+++ infra/rds/variables.tf
@@ -0,0 +1,4 @@
+variable "db_identifier"         { type = string }
+variable "region"                { type = string default = "us-west-2" }
+variable "db_instance_class"     { type = string default = "db.t3.micro" }
+variable "db_allocated_storage"  { type = number default = 20 }
