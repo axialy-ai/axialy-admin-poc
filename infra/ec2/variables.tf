a98fd3e4a88d11ac892d94ac9bf8072cf5ced3eb
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:15:57 -0700

    Update variables.tf

--- infra/ec2/variables.tf
+++ infra/ec2/variables.tf
@@ -1,26 +1,21 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
+  type    = string
+  default = "us-west-2"
 }
 
 variable "instance_name" {
-  description = "EC2 Name tag"
-  type        = string
+  type = string
 }
 
 variable "instance_type" {
-  description = "Instance size"
-  type        = string
-  default     = "t3.small"
+  type    = string
+  default = "t3.small"
 }
 
 variable "key_name" {
-  description = "Existing EC2 key-pair name"
-  type        = string
+  type = string
 }
 
 variable "elastic_ip_allocation_id" {
-  description = "Allocation-ID of an existing Elastic IP"
-  type        = string
+  type = string
 }
