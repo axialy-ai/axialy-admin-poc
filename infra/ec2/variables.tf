4d14b9fba51177a1812a21b36f3c9f5f79be6776
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:50:12 -0700

    Update variables.tf

--- infra/ec2/variables.tf
+++ infra/ec2/variables.tf
@@ -1,5 +1,26 @@
-variable "region"  { type = string }
-variable "instance_name"          { type = string }
-variable "instance_type"          { type = string }
-variable "key_name"               { type = string }
-variable "elastic_ip_allocation_id" { type = string }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "instance_name" {
+  description = "Tag / name for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing EC2 key-pair name for SSH access"
+  type        = string
+}
+
+variable "elastic_ip_allocation_id" {
+  description = "EIP allocation ID to associate with the instance"
+  type        = string
+}
