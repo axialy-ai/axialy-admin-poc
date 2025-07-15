7784b610f4c2eeeec5f40108bbc31288505e60dd
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:49:22 -0700

    Update variables.tf

--- infra/rds/variables.tf
+++ infra/rds/variables.tf
@@ -1,4 +1,17 @@
-variable "region"               { type = string }
-variable "db_identifier"        { type = string }
-variable "db_instance_class"    { type = string }
-variable "db_allocated_storage" { type = number }
+variable "region" {
+  description = "AWS region in which resources are created"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "db_user" {
+  description = "Master username for Postgres"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_port" {
+  description = "Postgres port"
+  type        = number
+  default     = 5432
+}
