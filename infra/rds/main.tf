a3b25adf5f3a1f53e1538c93f2cbf072bfbcd254
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:49:46 -0700

    Update main.tf

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -5,8 +5,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 ############################################
@@ -38,7 +41,7 @@ resource "aws_security_group" "rds" {
 ############################################
 resource "aws_db_subnet_group" "rds" {
   name       = "rds-subnet-group"
-  subnet_ids = data.aws_subnet_ids.default.ids
+  subnet_ids = data.aws_subnets.default.ids
 }
 
 resource "random_password" "db_password" {
