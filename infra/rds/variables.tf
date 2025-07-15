8636896539e5fc4f820f664f006687320ccb7d4f
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:17:20 -0700

    Update variables.tf

--- infra/rds/variables.tf
+++ infra/rds/variables.tf
@@ -1,22 +1,18 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
+  type    = string
+  default = "us-west-2"
 }
 
 variable "db_identifier" {
-  description = "Identifier prefix shared by the two DBs"
-  type        = string
+  type = string
 }
 
 variable "db_instance_class" {
-  description = "RDS instance class"
-  type        = string
-  default     = "db.t3.micro"
+  type    = string
+  default = "db.t3.micro"
 }
 
 variable "db_allocated_storage" {
-  description = "Allocated storage (GiB)"
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
