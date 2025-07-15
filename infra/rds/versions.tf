17be2cac37dc1b5c0734092b6fd6e37588c2e681
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:12:12 -0700

    Update versions.tf

--- infra/rds/versions.tf
+++ infra/rds/versions.tf
@@ -2,14 +2,8 @@ terraform {
   required_version = ">= 1.4.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
+    aws    = { source = "hashicorp/aws"    version = "~> 6.0" }
+    random = { source = "hashicorp/random" version = "~> 3.6" }
   }
 }
 
