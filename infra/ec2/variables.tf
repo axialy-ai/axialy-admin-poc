8b951f8537eef945bb4067f4c96f5af3220328a6
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:04:30 -0700

    Update variables.tf

--- infra/ec2/variables.tf
+++ infra/ec2/variables.tf
@@ -1,21 +1,33 @@
 variable "region" {
-  type    = string
-  default = "us-west-2"
+  description = "AWS region for the EC2 instance"
+  type        = string
+  default     = "us-west-2"
 }
 
 variable "instance_name" {
-  type = string
+  description = "Name tag for the EC2 instance"
+  type        = string
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t3.small"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
 }
 
 variable "key_name" {
-  type = string
+  description = "Existing EC2 key-pair name"
+  type        = string
 }
 
 variable "elastic_ip_allocation_id" {
-  type = string
+  description = "Allocation-ID of an existing Elastic IP (in the same region)"
+  type        = string
+}
+
+# Only needed if the account’s default VPC was deleted:
+variable "vpc_id" {
+  description = "Optional VPC ID to host the instance"
+  type        = string
+  default     = ""
 }
