22c72c2bc6723ddb2fcc2fce10d3a00b83a07fcc
Casey Lide <157076195+Axiamax@users.noreply.github.com>
2025-07-15 01:22:28 -0700

    Update variables.tf

--- infra/ec2/variables.tf
+++ infra/ec2/variables.tf
@@ -1,21 +1,5 @@
-variable "region" {
-  type    = string
-  default = "us-west-2"
-}
-
-variable "instance_name" {
-  type = string
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t3.small"
-}
-
-variable "key_name" {
-  type = string
-}
-
-variable "elastic_ip_allocation_id" {
-  type = string
-}
+variable "region"  { type = string }
+variable "instance_name"          { type = string }
+variable "instance_type"          { type = string }
+variable "key_name"               { type = string }
+variable "elastic_ip_allocation_id" { type = string }
